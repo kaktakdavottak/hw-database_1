129fb45f59677fc3d95929af3058311edb3b1a6c
kaktakdavottak <semenmineev@gmail.com>
2019-02-13 23:51:51 +0300

    Initial commit

---
+++ database_app.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+
+# создает таблицы
+def create_db():
+    pass
+
+# возвращает студентов определенного курса
+def get_students(course_id):
+    pass
+
+# создает студентов и
+# записывает их на курс
+def add_students(course_id, students):
+    pass
+
+# просто создает студента
+def add_student(student):
+    pass
+
+def get_student(student_id):
+    pass
